fcfb8d3235777ef0405301ab4fcc7fa0685bc15a
Kingsley <louis.officialz@gmail.com>
2023-11-29 00:58:10 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for char in range(ord('Z'), ord('a') - 1, -1):
-    print("{:c}".format(char), end='')
+i = 0
+for char in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(char - i)), end="")
+    i = 32 if i == 0 else 0
