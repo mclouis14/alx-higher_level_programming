efa6d3d5c6231df168d514bf1091874ff7c9f279
Kingsley <louis.officialz@gmail.com>
2023-12-06 16:15:46 -0800

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,7 +6,7 @@ def weight_average(my_list=[]):
     tot_score = 0
     tot_weight = 0
 
-    for score,weight in my_list:
+    for score, weight in my_list:
         tot_score += score * weight
         tot_weight += weight
 
