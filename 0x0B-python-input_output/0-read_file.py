043c9395047409179f8f61e3634f7e0ed536e856
Kingsley <louis.officialz@gmail.com>
2024-01-09 13:52:10 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,11 +4,5 @@
 
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
-    try:
-        with open(filename, 'r', encoding='utf-8')as file:
-            content = file.read()
-            print(content)
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
