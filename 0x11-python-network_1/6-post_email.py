b59599cb5e0e5f15342dea29298fce453a3e4efd
Kingsley <louis.officialz@gmail.com>
2024-04-01 04:47:26 -0700

    0x11 python-network_1 project

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,12 +3,10 @@
 Script takes in a URL and email address, sends a POST request to the passed URL
 with email as parameter and finally displays body of the response.
 """
-import sys
-import urllib.request
+import requests
+from sys import argv
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+if __name__ == '__main__':
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
+    print(r.text)
