69fb9f47411f30f36b9a0a14422739e44e777c57
Kingsley <louis.officialz@gmail.com>
2024-03-31 21:28:30 -0700

    Fixed issue on 4-header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script takes a URL as argument, sends GET request to the URL and displays body of the response.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
